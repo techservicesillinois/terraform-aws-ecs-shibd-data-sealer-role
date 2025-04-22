254e97f1cfa8803b57dd020970b2f90591d521a2
Jon R. Roma <jonroma@jonroma.net>
2025-04-22 12:11:33 -0500

    Support tags

--- variables.tf
+++ variables.tf
@@ -1,18 +1,24 @@
 variable "service" {
-  description = "The name of the service associated with the Shibboleth SP"
+  description = "Service name to associate with the Shibboleth SP"
 }
 
 variable "role_name" {
-  description = "The name of the created IAM role"
-  default     = ""
+  description = "IAM role name"
+  default     = null
 }
 
 variable "policy_name" {
-  description = "The name of the created IAM policy"
-  default     = ""
+  description = "IAM policy name"
+  default     = null
 }
 
 variable "secret_name" {
-  description = "The name of the Secrets Manager secret to allow access to"
-  default     = ""
+  description = "Secrets Manager secret to which to grant access"
+  default     = null
+}
+
+variable "tags" {
+  description = "Tags to be applied to resources where supported"
+  type        = map(string)
+  default     = {}
 }
